5e49f7c44750ca8f63642400f48eb7967b1d3f23
from20020516 <from20020516@gmail.com>
2021-01-12 02:36:00 +0900

    Add build dependencies.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 import { registerRootComponent } from 'expo';
-
+import 'babel-polyfill';
 import App from './App';
 
 // registerRootComponent calls AppRegistry.registerComponent('main', () => App);
