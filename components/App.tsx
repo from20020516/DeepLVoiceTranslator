c6f3d34ff953544f574c47ba2d5d9b9b392e3700
from20020516 <from20020516@gmail.com>
2021-01-14 00:19:14 +0900

    Change API access to via lambda function.

--- components/App.tsx
+++ components/App.tsx
@@ -2,10 +2,9 @@ import React, { FC, useEffect, useState } from 'react'
 import { ActivityIndicator, StyleSheet, View } from 'react-native'
 import { Card, Text } from 'react-native-elements'
 import { Picker } from '@react-native-picker/picker'
-import { auth_key, naver_client_id, naver_client_secret } from '@env'
+import { endpoint } from '@env'
 import axios from 'axios'
 import moment from 'moment'
-import querystring from 'querystring'
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition'
 import Translation from './Translation'
 
@@ -19,21 +18,19 @@ const styles = StyleSheet.create({
 
 const languages: Languages[] = [
   { language: "DE", name: "German" },
-  { language: "EN-GB", name: "English (British)" },
-  { language: "EN-US", name: "English (American)" },
+  { language: "EN", name: "English" },
   { language: "ES", name: "Spanish" },
   { language: "FR", name: "French" },
   { language: "IT", name: "Italian" },
   { language: "JA", name: "Japanese" },
   { language: "NL", name: "Dutch" },
   { language: "PL", name: "Polish" },
-  { language: "PT-PT", name: "Portuguese (European)" },
-  { language: "PT-BR", name: "Portuguese (Brazilian)" },
+  { language: "PT", name: "Portuguese" },
   { language: "RU", name: "Russian" },
   { language: "ZH", name: "Chinese" },
   { language: "KO", name: "Korean" }
 ]
-const initialLanguage = JSON.parse(localStorage.getItem('language') ?? '{"source":{"value":"JA","index":6},"target":{"value":"EN-US","index":2}}')
+const initialLanguage = JSON.parse(localStorage.getItem('language') ?? '{"source":{"value":"JA","index":6},"target":{"value":"EN","index":1}}')
 
 const App: FC = () => {
   const { interimTranscript } = useSpeechRecognition({
@@ -42,56 +39,29 @@ const App: FC = () => {
         command: '*',
         callback: async (command) => {
           if (command.length) {
-            try {
-              if (language.source.value === 'KO' || language.target.value === 'KO') {
-                const koTranslate = await axios.post<KoTranslationResult>('https://openapi.naver.com/v1/papago/n2mt', querystring.stringify({
-                  source: String(language.source.value).toLowerCase(),
-                  target: String(language.target.value).toLowerCase(),
-                  text: command
-                }), {
-                  headers: {
-                    'X-Naver-Client-Id': naver_client_id,
-                    'X-Naver-Client-Secret': naver_client_secret
-                  }
-                })
-                setTranslation([{
-                  time: moment().format('HH:mm:ss'),
-                  timestamp: moment().unix(),
-                  source: command,
-                  target: koTranslate.data.message.result.translatedText
-                }, ...translation])
-              } else {
-                const translate = await axios.post<Translation>('https://api.deepl.com/v2/translate', querystring.stringify({
-                  auth_key,
-                  text: command,
-                  target_lang: language.target.value,
-                }))
-                setTranslation([{
-                  time: moment().format('HH:mm:ss'),
-                  timestamp: moment().unix(),
-                  source: command,
-                  target: translate.data.translations[0].text
-                }, ...translation])
-                getApiUsage()
-              }
-            } catch (error) {
-              console.error(error)
-            }
+            const translate = await axios.post<string>(`${endpoint}/translate`, {
+              text: command,
+              target: language.target.value,
+              source: language.source.value
+            })
+            setTranslation([{
+              time: moment().format('HH:mm:ss'),
+              timestamp: moment().unix(),
+              source: command,
+              target: translate.data
+            }, ...translation])
           }
         },
         matchInterim: false
       },
     ]
   })
-  const [apiUsage, setApiUsage] = useState<ApiUsage>({ character_count: 0, character_limit: 0 })
   const [language, setLanguage] = useState<Language>(initialLanguage)
   const [translation, setTranslation] = useState<TranslationResult[]>([])
 
-  const getApiUsage = async () => setApiUsage((await axios.post<ApiUsage>('https://api.deepl.com/v2/usage', querystring.stringify({ auth_key }))).data)
   const startListening = () => SpeechRecognition.startListening({ language: language.source.value.toString(), continuous: true })
 
   useEffect(() => {
-    getApiUsage()
     return () => SpeechRecognition.abortListening()
   }, [])
 
@@ -131,9 +101,6 @@ const App: FC = () => {
       </View>
       <Card containerStyle={{ width: '80%' }}>
         <Card.Title style={{ height: 20 }}>{interimTranscript || <ActivityIndicator />}</Card.Title>
-        <Card.FeaturedSubtitle style={{ textAlign: 'right', color: 'darkgrey' }}>
-          {apiUsage?.character_count} / {apiUsage?.character_limit} ({(apiUsage?.character_count / apiUsage?.character_limit).toLocaleString('en-US', { style: 'percent' })})
-        </Card.FeaturedSubtitle>
         {translation.map((data) => (
           <Translation key={data.timestamp} data={data} />
         ))}
--- types/@env/index.d.ts
+++ types/@env/index.d.ts
@@ -1,3 +1,4 @@
 export const auth_key: string
+export const endpoint: string
 export const naver_client_id: string
 export const naver_client_secret: string
