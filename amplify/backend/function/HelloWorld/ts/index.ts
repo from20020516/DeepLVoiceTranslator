6d87f95e9995faf1d0d429dfbf759ace2c39897f
from20020516 <from20020516@gmail.com>
2021-01-13 20:52:06 +0900

    Fix HelloWorld lambda response type.

--- amplify/backend/function/HelloWorld/ts/index.ts
+++ amplify/backend/function/HelloWorld/ts/index.ts
@@ -1,10 +1,5 @@
-import { Callback, Context, Handler } from 'aws-lambda'
-interface TriggerEvent {
-    key1: string
-    key2: string
-    key3: string
-}
-export const handler: Handler<TriggerEvent, string> = (event: TriggerEvent, context: Context, callback: Callback<string>) => {
-    const concatKey = `${event.key1} ${event.key2} ${event.key3}`
-    callback(null, concatKey)
+import { APIGatewayProxyHandler } from 'aws-lambda'
+
+export const handler: APIGatewayProxyHandler = (event, context, callback) => {
+    callback(null, { statusCode: 200, body: JSON.stringify({ event, context }) })
 }
