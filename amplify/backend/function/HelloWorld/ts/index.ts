e5fac730ea11960656829fa2b45f064efdd63402
from20020516 <from20020516@gmail.com>
2021-01-13 23:15:05 +0900

    Remove HelloWorld lambda function.

--- amplify/backend/function/HelloWorld/ts/index.ts
+++
@@ -1,5 +0,0 @@
-import { APIGatewayProxyHandler } from 'aws-lambda'
-
-export const handler: APIGatewayProxyHandler = (event, context, callback) => {
-    callback(null, { statusCode: 200, body: JSON.stringify({ event, context }) })
-}
